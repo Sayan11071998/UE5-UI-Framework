36fbabe456990ae3564bde64a2533d2b91d4aa5d
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-10-29 22:32:07 +0530

    Widget Stack Gameplay Tags

---
+++ UE5_Frontend_UI/Source/UE5_Frontend_UI/Public/FrontendGameplayTags.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+	UE5_FRONTEND_UI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	UE5_FRONTEND_UI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	UE5_FRONTEND_UI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	UE5_FRONTEND_UI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+}
